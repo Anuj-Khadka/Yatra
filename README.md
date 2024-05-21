0ec08c7df58414ebaa952f7c73f2f453bc6880c6
Anuj-Khadka <anujkhadka008@gmail.com>
2024-05-21 16:01:31 -0400

    Initiate Project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Yatra
 This is an application for ease transportation.
+
+
+Color Palette:
+37CA3C
+F16262
+121212
+ffffff
--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/test-static')
+def test_static():
+    return f'<link rel="stylesheet" href="{url_for("static", filename="css/style.css")}"> Static file test.'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
