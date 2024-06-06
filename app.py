7b9d7f781e73294dc98861a2a72a3b6a39403418
Anuj-Khadka <anujkhadka008@gmail.com>
2024-06-05 22:47:04 -0400

    Update auth page

--- app.py
+++ app.py
@@ -15,5 +15,11 @@ def test_static():
 def signup():
     return render_template('signup.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
